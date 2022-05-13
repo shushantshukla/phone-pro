43c8e4d32be075820164066dec2b4c25ed110d2f
shushant shukla <uttamshukla113@gmail.com>
2022-05-13 13:08:54 +0530

    this is the summary report of the phone pro project
    nothing to say this is only a test file.

---
+++ pro.js
@@ -0,0 +1,24 @@
+//these are the variable section of the code;;;;;;
+var left_arr = document.getElementsByClassName("backarrow");
+var right_arr = document.getElementsByClassName("forwardarrow");
+var image = document.getElementsByClassName("first display")[0].id; 
+var imageele = document.getElementsByClassName("first display");
+
+
+let img_urls =[{ url:"/images/Realme9i_1.PNG"},
+                { url:"/images/Realme9i_2.PNG"}  ]
+// console.log(pr_image);
+left_arr.addEventListener('click', () => {
+   alert("hello in the loop");
+   // var i=0;
+   // if (image==1) { 
+   //    i++;
+   //    imageele[0].url = image[i].url
+   //    image++;
+      
+   // }
+
+});
+
+
+ 
